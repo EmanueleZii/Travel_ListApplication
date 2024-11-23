677ec58acd432b26c2e74cc4fb65e763b7b8bb2a
unknown <zignale92@gmail.com>
2024-11-23 10:38:50 +0100

    item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+const InitialItems = [
+  {id:1, description:"Passport", quantity:2, packed:false},
+  {id:2, description:"Socks", quantity:12, packed:false},
+];
+
 export default function App(){
  return (
  <div className="app">
-  <Logo />
-  <Form />
-  <PackingList />
-  <Stats />
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
   </div>
  );
 }
@@ -25,8 +30,24 @@ function Form(){
 
 function PackingList(){
   return (
-    <div className="list">List</div>
-    );
+    <div className="list">
+      <ul>
+      {InitialItems.map((item) => (
+        <Item item={item} />
+      ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({item})
+{
+ return <li>
+   <span style={item.packed ? {textDecoration:'line-through'} : {}}>
+    {item.quantity} {item.description}
+    </span>
+    <button className="btn">&times;</button>
+  </li>
 }
 
 function Stats(){
